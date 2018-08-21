49d0b1558c34e5092f64d467f67440d07be1d09d
Fabrinio Andes Santana Lemos <fabrinio_lemos@hotmail.com>
2018-08-21 00:16:04 -0300

    Lição 2 Encerrada

---
+++ Seção 06/App02_TipoPaginasXF/App02_TipoPaginasXF/App02_TipoPaginasXF/TipoPagina/Carrousel/Introducao_APP.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPaginasXF.TipoPagina.Carrousel
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Introducao_APP : CarouselPage
+	{
+		public Introducao_APP ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
---
+++ Seção 06/App02_TipoPaginasXF/App02_TipoPaginasXF/App02_TipoPaginasXF/TipoPagina/Carrousel/TipoPagina3.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPaginasXF.TipoPagina.Carrousel
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class TipoPagina3 : ContentPage
+	{
+		public TipoPagina3 ()
+		{
+			InitializeComponent ();
+		}
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+        }
+        private void MudarPagina(object sender, EventArgs e)
+        {
+            //App.Current.MainPage = new NavigationPage(new Navigation.Pagina1() { BackgroundColor = Color.Blue });
+            App.Current.MainPage = new Tabbed.Abas();
+        }
+    }
+}
