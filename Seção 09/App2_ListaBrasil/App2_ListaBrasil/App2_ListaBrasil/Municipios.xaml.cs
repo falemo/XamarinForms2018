309e65fabe02d47058af2986e8d7d89616afd75a
Fabrinio Andes Santana Lemos <fabrinio_lemos@hotmail.com>
2018-08-24 09:34:05 -0300

    Finalizado Exercicio

---
+++ Seção 09/App2_ListaBrasil/App2_ListaBrasil/App2_ListaBrasil/Municipios.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using App2_ListaBrasil.Modelo;
+
+namespace App2_ListaBrasil
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Municipios : ContentPage
+	{
+        private List<Municipio> ListaMunicipio { get; set; }
+        private List<Municipio> ListaFiltradaMunicipio { get; set; }
+
+        public Municipios (Estado estado)
+		{
+			InitializeComponent ();
+            ListaMunicipio = Servico.EstadoServico.GetMunicipio(estado.id);
+            ListaMunicipios.ItemsSource = ListaMunicipio;
+        }
+
+        private void BuscaRapida(object sender, TextChangedEventArgs args)
+        {
+            ListaFiltradaMunicipio = ListaMunicipio.Where(a => a.nome.Contains(args.NewTextValue)).ToList();
+            ListaMunicipios.ItemsSource = ListaFiltradaMunicipio;
+
+        }
+
+
+    }
+}
---
+++ Seção 09/App2_ListaBrasil/App2_ListaBrasil/App2_ListaBrasil/Servico/EstadoServico.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App2_ListaBrasil.Modelo;
+using Newtonsoft.Json;
+
+namespace App2_ListaBrasil.Servico
+{
+    public class EstadoServico
+    {
+        private static string url = "https://servicodados.ibge.gov.br/api/v1/localidades/estados";
+        private static string urlMunicipio = "https://servicodados.ibge.gov.br/api/v1/localidades/estados/{0}/municipios";
+
+        public static List<Estado> GetEstado()
+        {
+            WebClient ws = new WebClient();
+            string conteudo = ws.DownloadString(url);
+            return JsonConvert.DeserializeObject<List<Estado>>(conteudo);
+
+        }
+
+        public static List<Municipio> GetMunicipio(int idestado)
+        {
+            string newURL = string.Format(urlMunicipio, idestado);
+            WebClient ws = new WebClient();
+            string conteudo = ws.DownloadString(newURL);
+            return JsonConvert.DeserializeObject<List<Municipio>>(conteudo);
+
+        }
+
+    }
+}
